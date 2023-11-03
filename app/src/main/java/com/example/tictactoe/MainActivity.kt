61ddf9096bbbca578931fd75bb31fdffd129159d
Elias McKee <eqbookfan@gmail.com>
2023-11-02 21:43:54 -0700

    Added functionality to NewGameButton

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -13,6 +13,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val newGameButton = findViewById<Button>(R.id.NewGameButton)
+        newGameButton.setOnClickListener {
+            resetBoard()
+        }
+
+        // setting button click listeners
         val button1 = findViewById<Button>(R.id.Button1)
         val button2 = findViewById<Button>(R.id.Button2)
         val button3 = findViewById<Button>(R.id.Button3)
@@ -22,7 +28,6 @@ class MainActivity : AppCompatActivity() {
         val button7 = findViewById<Button>(R.id.Button7)
         val button8 = findViewById<Button>(R.id.Button8)
         val button9 = findViewById<Button>(R.id.Button9)
-
         button1.setOnClickListener {
             changeText(button1)
         }
@@ -52,8 +57,31 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun resetBoard() {
+        val button1 = findViewById<Button>(R.id.Button1)
+        button1.text = ""
+        val button2 = findViewById<Button>(R.id.Button2)
+        button2.text = ""
+        val button3 = findViewById<Button>(R.id.Button3)
+        button3.text = ""
+        val button4 = findViewById<Button>(R.id.Button4)
+        button4.text = ""
+        val button5 = findViewById<Button>(R.id.Button5)
+        button5.text = ""
+        val button6 = findViewById<Button>(R.id.Button6)
+        button6.text = ""
+        val button7 = findViewById<Button>(R.id.Button7)
+        button7.text = ""
+        val button8 = findViewById<Button>(R.id.Button8)
+        button8.text = ""
+        val button9 = findViewById<Button>(R.id.Button9)
+        button9.text = ""
+        val instructionText = this.findViewById<TextView>(R.id.InstructionText)
+        instructionText.text = "O's Turn"
+    }
+
     private fun changeText(button: Button) {
-        val instructionText = this.findViewById<TextView>(/* id = */ R.id.InstructionText)
+        val instructionText = this.findViewById<TextView>(R.id.InstructionText)
         if (instructionText.text.substring(0, 1) == "O") {
             button.text = "O"
             instructionText.text = "X's Turn"
