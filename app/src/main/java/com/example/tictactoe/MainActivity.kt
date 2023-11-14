4a4f1179fb59a8010dcc1d49bbf266476f8cf976
Elias McKee <eqbookfan@gmail.com>
2023-11-14 15:24:50 -0800

    Fixed implimentation of the win conditions, and changed the buttons, so that you can only click them when a game is started

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -17,44 +17,6 @@ class MainActivity : AppCompatActivity() {
         newGameButton.setOnClickListener {
             resetBoard()
         }
-
-        // setting button click listeners
-        val button1 = findViewById<Button>(R.id.Button1)
-        val button2 = findViewById<Button>(R.id.Button2)
-        val button3 = findViewById<Button>(R.id.Button3)
-        val button4 = findViewById<Button>(R.id.Button4)
-        val button5 = findViewById<Button>(R.id.Button5)
-        val button6 = findViewById<Button>(R.id.Button6)
-        val button7 = findViewById<Button>(R.id.Button7)
-        val button8 = findViewById<Button>(R.id.Button8)
-        val button9 = findViewById<Button>(R.id.Button9)
-        button1.setOnClickListener {
-            changeText(button1)
-        }
-        button2.setOnClickListener {
-            changeText(button2)
-        }
-        button3.setOnClickListener {
-            changeText(button3)
-        }
-        button4.setOnClickListener {
-            changeText(button4)
-        }
-        button5.setOnClickListener {
-            changeText(button5)
-        }
-        button6.setOnClickListener {
-            changeText(button6)
-        }
-        button7.setOnClickListener {
-            changeText(button7)
-        }
-        button8.setOnClickListener {
-            changeText(button8)
-        }
-        button9.setOnClickListener {
-            changeText(button9)
-        }
     }
 
     private fun resetBoard() {
@@ -77,42 +39,96 @@ class MainActivity : AppCompatActivity() {
         val button9 = findViewById<Button>(R.id.Button9)
         button9.text = ""
         val instructionText = this.findViewById<TextView>(R.id.InstructionText)
-        instructionText.text = "O's Turn"
+        instructionText.text = "X's Turn"
+        enableBoard()
+    }
+
+    private fun enableBoard() {
+        val button1 = findViewById<Button>(R.id.Button1)
+        button1.setOnClickListener { changeText(button1) }
+        val button2 = findViewById<Button>(R.id.Button2)
+        button2.setOnClickListener { changeText(button2) }
+        val button3 = findViewById<Button>(R.id.Button3)
+        button3.setOnClickListener { changeText(button3) }
+        val button4 = findViewById<Button>(R.id.Button4)
+        button4.setOnClickListener { changeText(button4) }
+        val button5 = findViewById<Button>(R.id.Button5)
+        button5.setOnClickListener { changeText(button5) }
+        val button6 = findViewById<Button>(R.id.Button6)
+        button6.setOnClickListener { changeText(button6) }
+        val button7 = findViewById<Button>(R.id.Button7)
+        button7.setOnClickListener { changeText(button7) }
+        val button8 = findViewById<Button>(R.id.Button8)
+        button8.setOnClickListener { changeText(button8) }
+        val button9 = findViewById<Button>(R.id.Button9)
+        button9.setOnClickListener { changeText(button9) }
+    }
+
+    private fun disableBoard() {
+        val button1 = findViewById<Button>(R.id.Button1)
+        button1.setOnClickListener { null }
+        val button2 = findViewById<Button>(R.id.Button2)
+        button2.setOnClickListener { null }
+        val button3 = findViewById<Button>(R.id.Button3)
+        button3.setOnClickListener { null }
+        val button4 = findViewById<Button>(R.id.Button4)
+        button4.setOnClickListener { null }
+        val button5 = findViewById<Button>(R.id.Button5)
+        button5.setOnClickListener { null }
+        val button6 = findViewById<Button>(R.id.Button6)
+        button6.setOnClickListener { null }
+        val button7 = findViewById<Button>(R.id.Button7)
+        button7.setOnClickListener { null }
+        val button8 = findViewById<Button>(R.id.Button8)
+        button8.setOnClickListener { null }
+        val button9 = findViewById<Button>(R.id.Button9)
+        button9.setOnClickListener { null }
     }
 
     private fun changeText(button: Button) {
         if (button.text == "") {
             val instructionText = this.findViewById<TextView>(R.id.InstructionText)
-            if (instructionText.text.substring(0, 1) == "O") {
-                button.text = "O"
-                instructionText.text = "X's Turn"
-            } else {
+            if (instructionText.text.substring(0, 1) == "X") {
                 button.text = "X"
-                instructionText.text = "O's Turn"
+                if (!isGameDone()) {
+                    instructionText.text = "O's Turn"
+                }
+            } else {
+                button.text = "0"
+                if (!isGameDone()) {
+                    instructionText.text = "X's Turn"
+                }
             }
-            isGameDone()
         }
     }
 
-    private fun isGameDone() {
-        if (threeInRow() || threeInColumn() || threeInDiagonal()) {
+    private fun isGameDone(): Boolean {
+        if (threeInRow("O") || threeInRow("X") || threeInColumn("O") || threeInColumn("X")
+            || threeInDiagonal("O") || threeInDiagonal("X")
+        ) {
             val instructionText = this.findViewById<TextView>(R.id.InstructionText)
             val player = instructionText.text.substring(0, 1)
             instructionText.text = "$player wins!"
+            disableBoard()
+            return true
         }
+        return false
     }
 
-    private fun threeInColumn(): Boolean {
+    private fun threeInColumn(text: String): Boolean {
         // if there is three in a row in one of the columns
         // buttons in the left column have the same text value
-        if ((findViewById<Button>(R.id.Button1).text == findViewById<Button>(R.id.Button4).text
-                    && findViewById<Button>(R.id.Button1).text == findViewById<Button>(R.id.Button7).text)
+        if ((findViewById<Button>(R.id.Button1).text == text
+                    && findViewById<Button>(R.id.Button4).text == text
+                    && findViewById<Button>(R.id.Button7).text == text)
             // buttons in the middle column have the same text value
-            || (findViewById<Button>(R.id.Button2).text == findViewById<Button>(R.id.Button5).text
-                    && findViewById<Button>(R.id.Button2).text == findViewById<Button>(R.id.Button8).text)
+            || (findViewById<Button>(R.id.Button2).text == text
+                    && findViewById<Button>(R.id.Button5).text == text
+                    && findViewById<Button>(R.id.Button8).text == text)
             // buttons in the right column have the same text value
-            || (findViewById<Button>(R.id.Button3).text == findViewById<Button>(R.id.Button6).text
-                    && findViewById<Button>(R.id.Button3).text == findViewById<Button>(R.id.Button9).text)
+            || (findViewById<Button>(R.id.Button3).text == text
+                    && findViewById<Button>(R.id.Button6).text == text
+                    && findViewById<Button>(R.id.Button9).text == text)
         ) {
             return true
         }
@@ -120,17 +136,20 @@ class MainActivity : AppCompatActivity() {
         return false
     }
 
-    private fun threeInRow(): Boolean {
+    private fun threeInRow(text: String): Boolean {
         // if there is three in a row in one of the rows
         // buttons in the top row have the same text value
-        if ((findViewById<Button>(R.id.Button1).text == findViewById<Button>(R.id.Button2).text
-                    && findViewById<Button>(R.id.Button1).text == findViewById<Button>(R.id.Button3).text)
-            // buttons in the middle row have the same text value
-            || (findViewById<Button>(R.id.Button4).text == findViewById<Button>(R.id.Button5).text
-                    && findViewById<Button>(R.id.Button4).text == findViewById<Button>(R.id.Button6).text)
-            // buttons in the bottom row have the same text value
-            || (findViewById<Button>(R.id.Button7).text == findViewById<Button>(R.id.Button8).text
-                    && findViewById<Button>(R.id.Button7).text == findViewById<Button>(R.id.Button9).text)
+        if ((findViewById<Button>(R.id.Button1).text == text
+                    && findViewById<Button>(R.id.Button2).text == text
+                    && findViewById<Button>(R.id.Button3).text == text)
+            // buttons in the middle column have the same text value
+            || (findViewById<Button>(R.id.Button4).text == text
+                    && findViewById<Button>(R.id.Button5).text == text
+                    && findViewById<Button>(R.id.Button6).text == text)
+            // buttons in the right column have the same text value
+            || (findViewById<Button>(R.id.Button7).text == text
+                    && findViewById<Button>(R.id.Button8).text == text
+                    && findViewById<Button>(R.id.Button9).text == text)
         ) {
             return true
         }
@@ -138,12 +157,15 @@ class MainActivity : AppCompatActivity() {
         return false
     }
 
-    private fun threeInDiagonal(): Boolean {
-        if ((findViewById<Button>(R.id.Button1).text == findViewById<Button>(R.id.Button5).text
-                    && findViewById<Button>(R.id.Button1).text == findViewById<Button>(R.id.Button9).text)
-            // buttons in the middle row have the same text value
-            || (findViewById<Button>(R.id.Button3).text == findViewById<Button>(R.id.Button5).text
-                    && findViewById<Button>(R.id.Button3).text == findViewById<Button>(R.id.Button7).text)
+    private fun threeInDiagonal(text: String): Boolean {
+        // buttons on the top-left to bottom-right diagonal have the same text value
+        if ((findViewById<Button>(R.id.Button1).text == text
+                    && findViewById<Button>(R.id.Button5).text == text
+                    && findViewById<Button>(R.id.Button9).text == text)
+            // buttons in the top-right to bottom-left have the same text value
+            || (findViewById<Button>(R.id.Button3).text == text
+                    && findViewById<Button>(R.id.Button5).text == text
+                    && findViewById<Button>(R.id.Button7).text == text)
         ) {
             return true
         }
